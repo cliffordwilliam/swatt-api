23ac7c6932654139fda58c52c532e2732b9de624
Clifford William <ccliffordwilliam@gmail.com>
2026-09-10 00:29:16 +0700

    feat: use nested transaction approach for session fixture
    This is the standard pattern to allow test to commit normally without
    messing the database state at all across other tests.

--- src/swatt_api/conftest.py
+++ src/swatt_api/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from alembic.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import NullPool
 
 from alembic import command
 from src.swatt_api.config import test_postgresql_url
@@ -9,7 +10,7 @@ from src.swatt_api.config import test_postgresql_url
 
 @pytest.fixture(scope="session")
 def test_engine():
-    test_engine = create_engine(test_postgresql_url)
+    test_engine = create_engine(test_postgresql_url, poolclass=NullPool)
     alembic_config = Config("alembic.ini")
     alembic_config.attributes["test_engine"] = test_engine
     command.upgrade(alembic_config, "head")
@@ -19,5 +20,12 @@ def test_engine():
 
 @pytest.fixture
 def test_session(test_engine):
-    with Session(test_engine) as test_session:
-        yield test_session
+    test_connection = test_engine.connect()
+    test_outer_transaction = test_connection.begin()
+    test_session = Session(
+        bind=test_connection, join_transaction_mode="create_savepoint"
+    )
+    yield test_session
+    test_session.close()
+    test_outer_transaction.rollback()
+    test_connection.close()

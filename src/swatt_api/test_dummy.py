f2be9837c23e953fbc0617a9fef1148e1969f370
Clifford William <ccliffordwilliam@gmail.com>
2026-09-10 00:52:04 +0700

    test: add a dummy test to prove the nested transaction approach works
    The test added here proves that each tests starts with a clean slate,
    the prior tests commits is never seen in other tests.

--- src/swatt_api/test_dummy.py
+++ src/swatt_api/test_dummy.py
@@ -1,2 +1,20 @@
-def test_dummy(test_session):
-    pass
+from sqlalchemy import text
+
+
+def test_add_record(test_session):
+    sql_instruction = text(
+        "INSERT INTO delivery_methods (name, created_at, updated_at) VALUES ('test', now(), now())"
+    )
+    test_session.execute(sql_instruction)
+    test_session.commit()
+    sql_instruction = text("SELECT count(*) FROM delivery_methods")
+    result = test_session.execute(sql_instruction)
+    number_of_existing_delivery_methods = result.scalar()
+    assert number_of_existing_delivery_methods == 1
+
+
+def test_sees_clean(test_session):
+    sql_instruction = text("SELECT count(*) FROM delivery_methods")
+    result = test_session.execute(sql_instruction)
+    number_of_existing_delivery_methods = result.scalar()
+    assert number_of_existing_delivery_methods == 0
